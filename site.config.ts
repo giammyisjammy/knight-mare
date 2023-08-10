cf935cbc4b5c26f9112a12e53d2540dc5378a339
giammyisjammy <49064602+giammyisjammy@users.noreply.github.com>
2023-08-10 12:59:41 +0200

    💄 Add custom links to header bar

--- site.config.ts
+++ site.config.ts
@@ -53,16 +53,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'Contatti',
+      pageId: '0397e7a16b304032940d68636ddb2d4a'
+    },
+    {
+      title: 'Iscrizioni 2024',
+      pageId: '299e27133664473dab769e2928783b6b'
+    }
+  ]
 })
