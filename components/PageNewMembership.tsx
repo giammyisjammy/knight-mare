7b62f8f85e8a4169e5c4a1f0194fc23d7dc729fb
giammyisjammy <49064602+giammyisjammy@users.noreply.github.com>
2023-08-28 16:03:22 +0200

    🚧 Work in progress

--- components/PageNewMembership.tsx
+++ components/PageNewMembership.tsx
@@ -4,28 +4,32 @@ import { api } from '@/lib/config'
 import fetcher from '@/lib/fetcher'
 
 import MembershipForm from './MembershipForm'
+import { PageHead } from './PageHead'
 
 export default function NewMembershipPage() {
   return (
-    <div className='notion-page'>
-      {/* <h1 className='notion-title'>Personal information</h1>
-      <p className='notion-text-block'>Type your personal information here</p> */}
-      <MembershipForm
-        className='notion-page-content'
-        mode='add'
-        onConfirm={async (formData) => {
-          console.log('New subscription', formData)
-          try {
-            const result = await fetcher(api.createNewMember, {
-              method: 'POST',
-              body: JSON.stringify(formData.serialize())
-            })
-            console.log('Success!', result)
-          } catch (error) {
-            console.log('Failed =(', error)
-          }
-        }}
-      />
-    </div>
+    <>
+      <PageHead title='🚧 Modulo Iscrizione Socio (under construction)' />
+      <div className='notion-page'>
+        {/* <h1 className='notion-title'>Personal information</h1>
+        <p className='notion-text-block'>Type your personal information here</p> */}
+        <MembershipForm
+          className='notion-page-content'
+          mode='add'
+          onConfirm={async (formData) => {
+            console.log('New subscription', formData)
+            try {
+              const result = await fetcher(api.createNewMember, {
+                method: 'POST',
+                body: JSON.stringify(formData.serialize())
+              })
+              console.log('Success!', result)
+            } catch (error) {
+              console.log('Failed =(', error)
+            }
+          }}
+        />
+      </div>
+    </>
   )
 }
