4d4fb6456b19fe416f0374c5ed05218e2b417d6d
giammyisjammy <49064602+giammyisjammy@users.noreply.github.com>
2024-01-28 00:03:39 +0100

    🚧 Work in progress

--- pages/api/create-new-member.tsx
+++ pages/api/create-new-member.tsx
@@ -1,17 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { Bot } from 'grammy'
 
 import { ClubMember } from '@/lib/ClubMember'
 import { createRegisterOfMemberEntry } from '@/lib/notion'
-import { getEnv } from '@/lib/get-config-value'
 import { notificationMessages } from '@/lib/get-notification-messages'
-
-// Create a new bot instance
-const bot = new Bot(getEnv('NEXT_TELEGRAM_TOKEN'))
-bot.start()
-
-const chat_id = Number(getEnv('NEXT_TELEGRAM_CHAT_ID'))
-const message_thread_id = Number(getEnv('NEXT_TELEGRAM_MESSAGE_THREAD_ID'))
+import { bot, CHAT_ID, MESSAGE_THREAD_ID } from '@/lib/telegram-bot'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== 'POST') {
@@ -19,28 +11,42 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     return res.status(405).send({ error: 'method not allowed' })
   }
 
+  const newClubMember = ClubMember.deserialize(JSON.parse(req.body))
+
+  let isSuccesfullyRegistered = false
   try {
     console.log('<<< typeof body', typeof req.body)
-    const newClubMember = ClubMember.deserialize(JSON.parse(req.body))
     console.log('<<< lambda create-database-entry', newClubMember)
 
-    const registrationResults = await createRegisterOfMemberEntry(newClubMember)
-    console.log('>>> lambda create-database-entry', registrationResults)
+    const results = await createRegisterOfMemberEntry(newClubMember)
+    console.log('>>> lambda create-database-entry', results.id)
 
-    // send telegram notification
-    const message = notificationMessages.newSubscription({
-      member: newClubMember,
-      isSuccesfullyRegistered: !!registrationResults.id,
-      isMailSent: false
-    })
-    await bot.api.sendMessage(chat_id, message, {
-      message_thread_id,
-      parse_mode: 'HTML'
-    })
+    isSuccesfullyRegistered = !!results.id
 
     res.status(200).json({ ok: true })
   } catch (error) {
     console.log('>>> da error', error)
     return res.status(400).send({ error: 'something bad happened' })
+  }
+
+  try {
+    // send telegram notification
+    const message = notificationMessages.newSubscription({
+      member: newClubMember,
+      isSuccesfullyRegistered
+    })
+    console.log('<<< lambda send-notification', {
+      CHAT_ID,
+      message,
+      message_thread_id: MESSAGE_THREAD_ID,
+      parse_mode: 'HTML'
+    })
+    const telegramResult = await bot.api.sendMessage(CHAT_ID, message, {
+      message_thread_id: MESSAGE_THREAD_ID,
+      parse_mode: 'HTML'
+    })
+    console.log('>>> lambda send-notification', telegramResult)
+  } catch (error) {
+    console.log('>>> da error', error)
   }
 }
--- pages/api/tlg.ts
+++
@@ -1,6 +0,0 @@
-export default async (req, res) => {
-  // const tgbot = process.env.NEXT_TELEGRAM_TOKEN
-  console.log(req.body)
-
-  res.status(200).send('OK')
-}
