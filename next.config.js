6ae80c8d84ed60f6e1e2079546bf3ce5ad522569
giammyisjammy <49064602+giammyisjammy@users.noreply.github.com>
2024-01-28 14:39:19 +0100

    🚧 Test for OG image generation

--- next.config.js
+++ next.config.js
@@ -6,18 +6,19 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 module.exports = withBundleAnalyzer({
   staticPageGenerationTimeout: 300,
   images: {
-    domains: [
-      'www.notion.so',
-      'notion.so',
-      'images.unsplash.com',
-      'pbs.twimg.com',
-      'abs.twimg.com',
-      's3.us-west-2.amazonaws.com',
-      'clubscacchicantu.it', // TODO domain name
-      'clubscacchicantu.com', // TODO domain name
-      'chessclubcantu.it', // TODO domain name
-      'chessclubcantu.com' // TODO domain name
-    ],
+    remotePatterns: [
+      { hostname: 'www.notion.so' },
+      { hostname: 'notion.so' },
+      { hostname: 'images.unsplash.com' },
+      { hostname: 'pbs.twimg.com' },
+      { hostname: 'abs.twimg.com' },
+      { hostname: 's3.us-west-2.amazonaws.com' },
+      { hostname: '**.vercel.app' },
+      { hostname: 'circoloscacchicantu.it' },
+      { hostname: 'circoloscacchicantu.com' },
+      { hostname: 'circoloscacchicantu.org' },
+      { hostname: 'cantuchess.club' }
+    ].map((pattern) => ({ ...pattern, protocol: 'https' })),
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
