f69c9fba97180af717b6c3ecfc4580313f93a901
giammyisjammy <49064602+giammyisjammy@users.noreply.github.com>
2024-01-28 01:19:18 +0100

    📈 Add Vercel speed analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import Document, { Head, Html, Main, NextScript } from 'next/document'
 
 import { IconContext } from '@react-icons/all-files'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 import { NoFlashScript } from '@/components/NoFlashScript'
 
@@ -23,10 +24,11 @@ export default class MyDocument extends Document {
           </Head>
 
           <body>
-            <NoFlashScript />
             <Main />
 
             <NextScript />
+            <NoFlashScript />
+            <SpeedInsights />
           </body>
         </Html>
       </IconContext.Provider>
